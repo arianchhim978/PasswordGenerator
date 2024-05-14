c4b7f9fe80e38e38e5ea66a3aeec6edb96d82c9d
arianchhim978 <147645634+arianchhim978@users.noreply.github.com>
2024-05-14 12:00:31 -0400

    created a password generator

---
+++ index.js
@@ -0,0 +1,24 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+
+let firstPasswordElement = document.getElementById("pElement");
+let secondPasswordElement = document.getElementById("pElement2");
+
+function generateRandomPass(length) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+    return password;
+}
+
+function generatePass() {
+    const firstPassword = generateRandomPass(12);
+    const secondPassword = generateRandomPass(12);
+
+    console.log("First Password:", firstPassword);
+    console.log("Second Password:", secondPassword);
+
+    firstPasswordElement.textContent = firstPassword;
+    secondPasswordElement.textContent = secondPassword;
+}
